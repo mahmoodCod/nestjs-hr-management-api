2f126151d55f8d04bc0e25a702274b0582e67e7c
mahmoodCod <nmashoomy11@gmail.com>
2026-02-16 16:59:12 +0330

    feat(departments-module): register employee controller and service in module
    - Register DepartmentsEmployeeController in DepartmentsModule controllers
    - Register DepartmentsEmployeeService in DepartmentsModule providers

--- hr-api/src/modules/departments/departments.module.ts
+++ hr-api/src/modules/departments/departments.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Department } from './entities/department.entity';
 import { DepartmentsManagerController } from './controllers/manager.department.controller';
 import { DepartmentsManagerService } from './services/manager.department.service';
+import { DepartmentsEmployeeController } from './controllers/employee.department.controller';
+import { DepartmentsEmployeeService } from './services/employee.department.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Department])],
 
-  controllers: [DepartmentsManagerController],
-  providers: [DepartmentsManagerService],
+  controllers: [DepartmentsManagerController, DepartmentsEmployeeController],
+  providers: [DepartmentsManagerService, DepartmentsEmployeeService],
 })
 export class DepartmentModule {}
