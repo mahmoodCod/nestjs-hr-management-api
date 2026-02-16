ff76f8f74e7c4a07cf710153adca20513011a6eb
mahmoodCod <nmashoomy11@gmail.com>
2026-02-16 08:57:48 +0330

    feat(departments): add UpdateDepartmentDto
    - Add @nestjs/mapped-types dependency
    - Create UpdateDepartmentDto using PartialType from CreateDepartmentDto
    - Import CreateDepartmentDto in manager department service

--- hr-api/src/modules/departments/services/manager.department.service.ts
+++ hr-api/src/modules/departments/services/manager.department.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Department } from '../entities/department.entity';
 import { Repository } from 'typeorm';
+import { CreateDepartmentDto } from '../dto/create.department.dto';
 
 @Injectable()
 export class DrepertmentManagerService {
