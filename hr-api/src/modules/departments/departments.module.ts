ca7a4cf2355e60867aa75cd0dc775cd617666497
mahmoodCod <nmashoomy11@gmail.com>
2026-02-16 14:02:37 +0330

    feat(departments-module): wire department module into app and register manager layer
    - Import DepartmentModule into AppModule to expose department features
    - Register DepartmentsManagerController in DepartmentsModule controllers
    - Register DepartmentsManagerService in DepartmentsModule providers

--- hr-api/src/modules/departments/departments.module.ts
+++ hr-api/src/modules/departments/departments.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Department } from './entities/department.entity';
+import { DepartmentsManagerController } from './controllers/manager.department.controller';
+import { DepartmentsManagerService } from './services/manager.department.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Department])],
 
-  controllers: [],
-  providers: [],
+  controllers: [DepartmentsManagerController],
+  providers: [DepartmentsManagerService],
 })
 export class DepartmentModule {}
