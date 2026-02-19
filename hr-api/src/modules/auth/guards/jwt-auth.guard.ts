b644f40689073e5f7afbe6c44a8eb7c6e0108c1e
mahmoodCod <nmashoomy11@gmail.com>
2026-02-19 15:57:59 +0330

    feat(auth-guard): add @Public decorator support to bypass jwt authentication
    Enhance JwtAuthGuard to read IS_PUBLIC_KEY metadata using Reflector and conditionally skip authentication for public routes
    Introduce @Public custom decorator to explicitly mark endpoints as publicly accessible without JWT validation
    Improve flexibility of route-level access control without affecting protected endpoints

--- hr-api/src/modules/auth/guards/jwt-auth.guard.ts
+++ hr-api/src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,6 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 // Passport JWT guard that protects routes by validating the access token.
 @Injectable()
-export class JwtAurhGuard extends AuthGuard('jwt') {}
+export class JwtAurhGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    // Checking the public decorator
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    // If the root is public, we skip authentication
+    if (isPublic) {
+      return true;
+    }
+
+    // Otherwise, we run normal authentication
+    return super.canActivate(context);
+  }
+}
