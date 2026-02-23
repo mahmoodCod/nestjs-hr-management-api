1cfeecadcfe18bb7db8f4861b12b8952dd2f9a24
mahmoodCod <nmashoomy11@gmail.com>
2026-02-23 08:58:57 +0330

    feat(attendance-service): add employee check-out functionality
    Extend AttendanceEmployeeService with `checkOut` method to update the latest attendance record without a check-out time, append optional notes, and save the updated record
    Ensures proper tracking of employee check-out events and maintains complete attendance history

--- hr-api/src/modules/attendences/services/attendance-employee.service.ts
+++ hr-api/src/modules/attendences/services/attendance-employee.service.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Attendance } from '../entities/attendance.entity';
-import { Repository } from 'typeorm';
+import { IsNull, Repository } from 'typeorm';
 
 @Injectable()
 export class AttendanceEmployeeService {
@@ -18,6 +18,29 @@ export class AttendanceEmployeeService {
       checkInTime: new Date(),
       notes: notes || null,
     });
+
+    return await this.attendanceRep.save(attendance);
+  }
+
+  async checkOut(userId: number, notes?: string) {
+    const attendance = await this.attendanceRep.findOne({
+      where: {
+        user: { id: userId },
+        checkOutTime: IsNull(),
+      },
+      order: { checkInTime: 'DESC' },
+    });
+
+    if (!attendance)
+      throw new NotFoundException('No input records found with no output');
+
+    attendance.checkOutTime = new Date();
+
+    if (notes) {
+      attendance.notes = attendance.notes
+        ? `${attendance.notes} - ${notes}`
+        : notes;
+    }
 
     return await this.attendanceRep.save(attendance);
   }
