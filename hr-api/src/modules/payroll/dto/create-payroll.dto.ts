a0aaea7d779a68036bd3cc149912848f4f657da0
mahmoodCod <nmashoomy11@gmail.com>
2026-02-25 09:17:43 +0330

    feat(payroll-create.dto): add salaryPeriod and baseSalary validations
    - Add `salaryPeriod` field with string type, required validation, and regex format YYYY/MM
    - Add `baseSalary` field with number type, minimum value 0, and required validation
    - Include Swagger `ApiProperty` for both fields for API documentation

--- hr-api/src/modules/payroll/dto/create-payroll.dto.ts
+++ hr-api/src/modules/payroll/dto/create-payroll.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsNotEmpty, IsNumber, Min } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, Matches, Min } from 'class-validator';
 
 export class CreatePayrollDto {
   @ApiProperty({
@@ -12,4 +12,23 @@ export class CreatePayrollDto {
   @Min(1, { message: 'ID must be greater than 0' })
   @IsNotEmpty({ message: 'ID is required' })
   userId: number;
+
+  @ApiProperty({
+    example: '1404/08',
+    description: 'Payroll month and year',
+  })
+  @IsString({ message: 'Month and year must be a string' })
+  @Matches(/^\d{4}\/\d{2}$/, { message: 'Invalid date format. Use YYYY-MM' })
+  @IsNotEmpty({ message: 'Month and year is required' })
+  salaryPeriod: string;
+
+  @ApiProperty({
+    example: 5000000,
+    description: 'Basic rights',
+  })
+  @Type(() => Number)
+  @IsNumber({}, { message: 'Basic rights must be a number' })
+  @Min(0, { message: 'Basic rights must be greater than 0' })
+  @IsNotEmpty({ message: 'Basic rights is required' })
+  baseSalary: number;
 }
