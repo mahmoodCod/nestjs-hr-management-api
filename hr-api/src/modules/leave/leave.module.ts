3f09c3fd3a8015688544255de630906af7c6eae7
mahmoodCod <nmashoomy11@gmail.com>
2026-05-08 17:51:23 +0330

    feat(leave-module): register manager leave type controller in module
    - Import ManagerLeaveTypeController into LeaveModule
    - Add ManagerLeaveTypeController to leave module controllers list
    - Keep leave request/type repositories wired via TypeOrmModule.forFeature

--- hr-api/src/modules/leave/leave.module.ts
+++ hr-api/src/modules/leave/leave.module.ts
@@ -5,10 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LeaveRequest } from './entities/leave-request.entity';
 import { LeaveType } from './entities/leave-type.entity';
 import { ManagerLeaveController } from './controllers/manager.leave.controller';
+import { ManagerLeaveTypeController } from './controllers/manager.leave.type.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([LeaveRequest, LeaveType])],
-  controllers: [EmployeeLeaveController, ManagerLeaveController],
+  controllers: [
+    EmployeeLeaveController,
+    ManagerLeaveController,
+    ManagerLeaveTypeController,
+  ],
   providers: [LeaveService],
   exports: [LeaveService],
 })
