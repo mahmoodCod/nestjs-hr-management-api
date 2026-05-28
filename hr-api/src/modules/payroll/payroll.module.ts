f751f9174204c74105547ac685b1eaa2c15eee79
mahmoodCod <nmashoomy11@gmail.com>
2026-05-28 05:15:20 +0330

    feat(payroll): register employee payroll controller in PayrollModule

--- hr-api/src/modules/payroll/payroll.module.ts
+++ hr-api/src/modules/payroll/payroll.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payroll } from './entities/payroll.entity';
 import { PayrollManagerController } from './controllers/payroll-manager.controller';
+import { PayrollEmployeeController } from './controllers/payroll-employee.controller';
 import { PayrollManagerService } from './services/payroll-manager.service';
 import { User } from '../auth/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Payroll, User])],
 
-  controllers: [PayrollManagerController],
+  controllers: [PayrollManagerController, PayrollEmployeeController],
 
   providers: [PayrollManagerService],
 })
